c9c562a327663b5074d7a1c2960ed46ba01cca82
artem <artemisamiddinov@gmail.com>
2026-05-12 22:59:05 +0000

    main feat: overwritten ci yml and add statevalidator interface comment

--- src/statevalidatorinterface.php
+++ src/statevalidatorinterface.php
@@ -6,6 +6,9 @@ namespace Demai\BusinessProcess;
 
 use Demai\BusinessProcess\Entity;
 
+/**
+ * Интерфейс валидатора перехода на стадию для сущности
+ */
 interface StateValidatorInterface
 {
     public function validate(Entity\EntityInterface $entity): bool|string;
